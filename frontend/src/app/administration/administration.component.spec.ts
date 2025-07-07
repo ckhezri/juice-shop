c2e2bceaa433379f6fa0d2aaeafebc989e792a83
Camran Khezri <ckhezri@gmail.com>
2025-07-07 11:56:45 -0400

    Initial commit

--- frontend/src/app/administration/administration.component.spec.ts
+++ frontend/src/app/administration/administration.component.spec.ts
@@ -55,7 +55,7 @@ describe('AdministrationComponent', () => {
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting()
       ]
-    })
+    })s
       .compileComponents()
   }))
 
